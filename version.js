891bb1343e8d4d80aacb2685d864b6edf6b887b0
uqamsavatelier <rivet.nicholas@uqam.ca>
2026-04-17 08:13:48 -0400

    Add files via upload

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.9.11";
+export const APP_VERSION = "2.9.12";
 
 document.querySelectorAll(".app-version").forEach((el) => {
   el.textContent = `Version ${APP_VERSION}`;
